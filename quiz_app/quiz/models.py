6de41e6093c02f13c339e37ff049329dc98c39d1
sparshdang <dang.sparsh01@gmail.com>
2023-10-25 21:53:57 +0530

    Created basic models

---
+++ quiz_app/quiz/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Quiz(models.Model):
+    code = models.CharField(max_length=10)
+    password = models.CharField(max_length=50)
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name="questions")
+    text = models.TextField(max_length=200)
+
+class Choices(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='choices')
+    text = models.CharField(max_length=50)
+    is_correct = models.BooleanField(default=False)
